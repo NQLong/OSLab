a43e5b7f26a12d22076195f491ad7f35aefaa429
NQLong <62022783+NQLong@users.noreply.github.com>
2020-04-16 17:24:52 +0700

    Update main.c

--- lab2/main.c
+++ lab2/main.c
@@ -9,15 +9,26 @@
 int main (int argc, char* argv[]){
 //Implementmygrep
     char substr[MAX_LEN];
-    char* str=(char *)alloc(sizeof(char));
-    
-    int x=0,y=0;
-    do  { 
-        x =read_line(str);
-        //x = find_sub_string(str,substr);
-        printf("%s\n",str);
-        
-
+    char str[MAX_LEN][MAX_LEN];
+    int line=0;
+    int len=0;
+    int i =0;
+    int temp=0;
+    do {
+        len = read_line(str[line++]);
+        printf("%s\n", str[line-1]);
     }
-    while (x!=-1);
+    while (len!=-1);
+    
+    fgets(substr, MAX_LEN, stdin);
+    while (1) {
+        if (substr[i]=='\n')
+            substr[i]='\0';
+    } 
+    
+    for (i = 0; i < len; i++){
+        temp = find_sub_string(str[i], substr);
+        if (temp!=0) printf("%s\n", str[i]);   
+    }
+    return 0;
 }
