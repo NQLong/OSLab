7cfd49f001fcb23936b8caf8939cd9a174fad792
Ben <nqlong2017@gmail.com>
2020-04-27 22:01:35 +0700

    test

--- lab3/test.c
+++ lab3/test.c
@@ -5,7 +5,7 @@ int main() {
     pid_t i =fork();
     pid_t k =fork();
     pid_t u=fork();
-    printf("%d",getpid());
+    printf("%d\n",getpid());
     pid_t test = getpid();
     printf("%d %d %d\n\n",i,u,k);
 }
