e220bde9509dec7cbe95d5dfe84d729be08b8a75
Ben <nqlong2017@gmail.com>
2020-04-27 22:00:07 +0700

    test

--- lab3/test.c
+++ lab3/test.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 int main() {
-    int i =fork();
-    i=fork();
-    i=fork();
+    pid_t i =fork();
+    pid_t k =fork();
+    pid_t u=fork();
     printf("%d",getpid());
     pid_t test = getpid();
-    printf("%d\n\n",test);
+    printf("%d %d %d\n\n",i,u,k);
 }
