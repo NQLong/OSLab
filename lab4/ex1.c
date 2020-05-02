a60070962fa1502f0705ca4c3d3fb9844bcb3a48
Ben <nqlong2017@gmail.com>
2020-05-03 01:58:49 +0700

    test lab4

--- lab4/ex1.c
+++ lab4/ex1.c
@@ -5,10 +5,14 @@ void* aligned_malloc (unsigned int size , unsigned int align ) {
     
     ptr = sbrk(0);
     printf("%d\n", &*ptr);
-    while ((int)&*ptr % align != 0) {
+    while ((int)&*ptr % align != 0 || sbrk(size) == (void*)-1) {
         &*ptr++;
     }
-    printf("%d", &*ptr);
+    printf("%d\n", &*ptr);
+    ptr = sbrk(0);
+    printf("%d\n", &*ptr);
+
+    
 }
 
 void* aligned_free (void *ptr){
