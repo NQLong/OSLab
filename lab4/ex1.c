2c5ec0e82f87aa83425eef5f1ffda508bd4d8107
Ben <nqlong2017@gmail.com>
2020-05-03 02:29:15 +0700

    test lab4

--- lab4/ex1.c
+++ lab4/ex1.c
@@ -1,17 +1,22 @@
 #include "ex1.h"
 
+void getaddress(void *ptr, unsigned int align){
+    while ((int)&*ptr % align != 0)
+        &*ptr++;
+    
+}
+
 void* aligned_malloc (unsigned int size , unsigned int align ) {
     void * ptr;
+    ptr = sbrk(0);
+    printf("%d\n", &*ptr);
+    getaddress(ptr,align);
+    printf("%d\n", &*ptr);
+    while (sbrk(size)==(void*)-1) 
+        getaddress(ptr,align);
+    ptr = sbrk(0);
+    printf("%d\n", &*ptr);
     
-    ptr = sbrk(0);
-    printf("%d\n", &*ptr);
-    while (!((int)&*ptr % align == 0 && sbrk(size) != (void*)-1)) {
-        &*ptr++;
-    }
-    printf("%d\n", &*ptr);
-    ptr = sbrk(0);
-    printf("%d\n", &*ptr);
-
     
 }
 
--- lab4/ex1.h
+++ lab4/ex1.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void getaddress(void *ptr, unsigned int align);
 void* aligned_malloc (unsigned int size , unsigned int align ) ;
 
 void* aligned_free (void *ptr);
