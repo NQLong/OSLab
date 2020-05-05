939fd7771367311e1c55d2cfd7127c16f8bd47ac
Ben <nqlong2017@gmail.com>
2020-05-05 22:39:38 +0700

    new model

--- lab4/ex1-3.c
+++ lab4/ex1-3.c
@@ -82,7 +82,8 @@ int main(){
     printf("%d ptr2 address\n",&*ptr2);
     printf("_____________________________________________________\n");
     printf("_____________________________________________________\n");
-    ((struct myMem*)Head)->status =0;
+    ((struct myMem*)Head)->status = 0;
+    printf("hello");
     void *ptr3 = aligned_malloc(30,5);
     printf("%d ptr3 address\n",&*ptr3);
     // void *ptr2 = aligned_malloc(30,5);
