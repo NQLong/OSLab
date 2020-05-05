5d68cdaba61cc7ed2cb9c0e9e9e82a41b90d4e14
Ben <nqlong2017@gmail.com>
2020-05-06 00:52:29 +0700

    makefile

--- lab4/main.c
+++ lab4/main.c
@@ -3,10 +3,11 @@
 int main(){
     printf("first call,create an dynamic array with 4 elements and set their values to 0,1,3,4\n");
     int* ptr= (int*)aligned_malloc(sizeof(int)*4,1024);
-    for (int i =0;i<4;i++){
+    int i =0;
+    for (i =0;i<4;i++){
         ptr[i]=i;
     }
-    for (int i =0;i<4;i++){
+    for (i =0;i<4;i++){
         pritnf("%d ",ptr[i]);
     }
     printf("\n");
