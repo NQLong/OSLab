234e72e648ba30ce50ba9e814e663546c471a353
Ben <nqlong2017@gmail.com>
2020-05-06 01:07:01 +0700

    h

--- lab4/main.c
+++ lab4/main.c
@@ -1,7 +1,7 @@
 #include "ex1.h"
 
 int main(){
-    printf("first call,create an dynamic array with 4 elements and set their values to 0,1,3,4\n");
+    printf("first call,create an dynamic array with 4 elements and align 1024 \"ptr\" and set their values to 0,1,2,3\n");
     int* ptr= (int*)aligned_malloc(sizeof(int)*4,1024);
     int i =0;
     for (i =0;i<4;i++){
@@ -11,6 +11,15 @@ int main(){
         printf("%d ",ptr[i]);
     }
     printf("\n");
-    printf("%d\n",&*ptr);
+    printf("ptr 1st byte address %zd\n",&*ptr);
+    printf("second call,create a new array of char (100 element, 1024 align");
+    printf("ptr2 1st byte address %zd\n",&*ptr2);
+    char* ptr2= (char*)aligned_malloc(sizeof(char)*100,1024);
+    printf("releasing ptr...\n");
+    ptr = aligned_free(ptr);
+    printf("third call, make a void pointer with size of 1bytes and align 1024- it should point to the address which ptr point to before we free it\n");
+    void* ptr3 = aligned_malloc(1,1024);
+    printf("ptr3 1st byte address %zd\n",&*ptr3);
+
     return 0;
 }
