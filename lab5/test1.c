dc409540f8541c2ffa03263b126a0dae4346db6c
Ben <nqlong2017@gmail.com>
2020-05-12 13:02:44 +0700

    test1

---
+++ lab5/test1.c
@@ -0,0 +1,26 @@
+#include <pthread.h>
+#include <stdio.h>
+#define NUM_THREADS 5
+void *PrintHello(void* threadid)
+{
+    longtid;
+    tid=(long)threadid;
+    printf("Hello World! It’s me, thread #%ld!\n",tid);
+    pthread_exit(NULL);
+}
+int main(int argc,char *argv[])
+{
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    for(t=0;t<NUM_THREADS;t++){
+        printf("Inmain:creatingthread%ld\n",t);
+        rc = pthread_create(&threads[t],NULL,PrintHello,(void*)t);
+        if(rc){
+            printf("ERROR;returnfrompthread_create()is%d\n",rc);
+            exit(−1);
+        }
+    }
+    /*Last thing that main() should do*/
+    pthread_exit(NULL);
+}
