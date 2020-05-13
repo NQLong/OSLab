6580c7e1228bfd5f27bbfb7cef3069ec20b1f374
Ben <nqlong2017@gmail.com>
2020-05-13 11:38:29 +0700

    test

--- lab5/ex2.c
+++ lab5/ex2.c
@@ -3,15 +3,15 @@
 pthread_mutex_t lock;
 
 void *hello (void *tid ) {
-    pthread_mutex_lock(&lock);
+
     printf("Hello from thread %d\n" , (int)tid ) ;
-    pthread_mutex_unlock(&lock);
+
 }
 int main() {
     pthread_t tid [10] ;
     int i;
     for ( i = 0; i < 10; i++) {
-       
+        if (i>0) pthread_join(tid[i-1],NULL);
         pthread_create(&tid[i] , NULL, hello , (void*)i) ;
     }
     pthread_exit (NULL);
