8567487ef189ce736ea2ac5040b456934c44293b
NQLong <nqlong0709@gmail.com>
2020-06-01 21:37:58 +0700

    as

--- asignment/sys_procmem.c
+++ asignment/sys_procmem.c
@@ -1,4 +1,7 @@
 #include <linux/linkage.h>
+#include <linux/sched.h>
+#include <linux/kernel.h>
+
 struct proc_segs {
 unsigned long mssv;
 unsigned long start_code;
@@ -9,6 +12,20 @@ unsigned long start_heap;
 unsigned long end_heap;
 unsigned long start_stack;
 };
-void sys_procmem( int pid, struct proc_segs* info) {
-    struct task
+
+asmlinkage long sys_procmem( int pid, struct proc_segs * info) {
+    struct task_truct *temp;
+    temp = find_task_by_vpid(pid);
+    if (!temp) return 1;
+    info->mssv = 1812917;
+    struct mm_struct *pmm;
+    pmm = temp->mm;
+    info->start_code = pmm->start_code;
+    info->start_data = pmm->start_data;
+    info->start_heap = pmm->start_brk;
+    info->start_stack = pmm->start_stack;
+    info->end_code = pmm->end_code;
+    info->end_data = pmm->end_data;
+    info->end_heap = pmm->sbrk;
+    return 1;
 }
